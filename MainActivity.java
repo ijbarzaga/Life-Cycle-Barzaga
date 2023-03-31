035da64c2dc8b337992776f76eac5e89ed3f6822
Ivan Jay Barzaga <116327722+ijbarzaga@users.noreply.github.com>
2023-03-31 18:40:57 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,69 +1,53 @@
 package com.example.lifecycle;
 
-import static android.widget.Toast.*;
-
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.lifecycle.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Bundle containing previous frozen state
         setContentView(R.layout.activity_main);
-
-        // The content view pointing to the id of layout
-        // in the file activity_main.xml
-        Toast.makeText(getApplicationContext(), "onCreate Called", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Created!", Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "Stopped!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "Resumed!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(getApplicationContext(), "onStart Called", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext() ,"Started!", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(getApplicationContext(), "onRestart Called", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Restarted!", Toast.LENGTH_LONG).show();
     }
 
+    @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(getApplicationContext(), "onPause Called", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Paused!", Toast.LENGTH_LONG).show();
     }
 
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getApplicationContext(), "onResume Called", Toast.LENGTH_LONG).show();
-    }
-
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(), "onStop Called", Toast.LENGTH_LONG).show();
-    }
-
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getApplicationContext(), "onDestroy Called", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Destroyed!", Toast.LENGTH_SHORT).show();
     }
 }
 
